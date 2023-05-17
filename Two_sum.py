ac38dcaa5146c22ef7e61b093d50f6c805e3ec24
StouAi <129324332+StouAi@users.noreply.github.com>
2023-05-17 18:03:29 +0300

    Create Two_sum.py

---
+++ Two_sum.py
@@ -0,0 +1,10 @@
+def twoSumsimple(nums, target):
+    output = []
+    for i in range(0, len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                output.append(i)
+                output.append(j)
+                return output
+              
+             
